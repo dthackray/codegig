963f13ad83bc0d92bcc5485ec299d9d88d3bb7de
Dan Thackray <djthackray@gmail.com>
2022-08-31 12:02:38 +0100

    Initialise Sequelize
    Signed-off-by: Dan Thackray <djthackray@gmail.com>

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ const exphbs = require("express-handlebars")
 const bodyParser = require("body-parser")
 const path = require("path")
 
+const Sequelize = require("sequelize")
+const db = new Sequelize ("codegig", "postgres", "password", {
+    host: "localhost",
+    dialect: "postgres",
+    operatorsAliases: false,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+})
+
 const app = express()
 
 app.get("/", (req, res) => res.send("INDEX"))
