0f9796b1b70fcd4fd789713319a4640827b3ac47
Dan Thackray <djthackray@gmail.com>
2022-09-02 14:08:38 +0100

    Test gig submission
    Signed-off-by: Dan Thackray <djthackray@gmail.com>

--- routes/gigs.js
+++ routes/gigs.js
@@ -47,6 +47,15 @@ router.post("/add", (req, res) => {
             contact_email
         })
     } else {
+        if (!budget) {
+            budget = "Unknown"
+        } else {
+            budget = `$${budget}`
+        }
+
+        // Make lower case and remove space after comma
+        technologies = technologies.toLowerCase().replace(/, /g, ",")
+
         // Insert into table
         Gig.create({
             title,
